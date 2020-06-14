5c9aa1d945cbe124e2a9c3810529c024b4724288
John Humble <johnhhumble@gmail.com>
2020-06-14 17:16:18 -0600

    changed tab size to 2 for each file

--- source/entities.js
+++ source/entities.js
@@ -3,32 +3,32 @@ var sheildImage = new Image();
 sheildImage.src = "sprites/sheild.png";
 
 function drawSheild(sheild, x, y, tarX, tarY) {
-    let offsetX = 0; 
-    let offsetY = 0;
-    let image = sheild.image;
+  let offsetX = 0; 
+  let offsetY = 0;
+  let image = sheild.image;
 
-    let rot = Math.atan2(tarY - y, tarX - x);
+  let rot = Math.atan2(tarY - y, tarX - x);
 
-    if (sheild.active){
-        sheild.x = tileSize/2 * Math.cos(rot) + x;
-        sheild.y = tileSize/2 * Math.sin(rot) + y;
-    }
-    else {
-        sheild.x = -tileSize/3 * Math.cos(rot) + x;
-        sheild.y = -tileSize/3 * Math.sin(rot) + y;
-    }
+  if (sheild.active){
+    sheild.x = tileSize/2 * Math.cos(rot) + x;
+    sheild.y = tileSize/2 * Math.sin(rot) + y;
+  }
+  else {
+    sheild.x = -tileSize/3 * Math.cos(rot) + x;
+    sheild.y = -tileSize/3 * Math.sin(rot) + y;
+  }
 
-    ctx.drawImage(image,sheild.x - tileSize/2,sheild.y - tileSize/2,tileSize,tileSize);
+  ctx.drawImage(image,sheild.x - tileSize/2,sheild.y - tileSize/2,tileSize,tileSize);
 }
 
 function createSteelSheild(){
-    return {
-        name: "Steel Sheild",
-        x: 0,
-        y: 0,
-        active: false,
-        image: sheildImage
-    }
+  return {
+    name: "Steel Sheild",
+    x: 0,
+    y: 0,
+    active: false,
+    image: sheildImage
+  }
 }
 
 // WEAPONS
@@ -45,96 +45,96 @@ var swordSteelLD = new Image();
 swordSteelLD.src = "sprites/swordsteelLD.png";
 
 function drawWeapon(weapon,x, y, tarX, tarY){
-    let offsetX = 0;
-    let offsetY = 0;
-    let image = weapon.rd;
+  let offsetX = 0;
+  let offsetY = 0;
+  let image = weapon.rd;
 
-    let left = false;
-    let up = false;
-    
-    let rot = Math.atan2(tarY - y, tarX - x);
+  let left = false;
+  let up = false;
+  
+  let rot = Math.atan2(tarY - y, tarX - x);
 
-    weapon.x = tileSize * Math.cos(rot) + x;
-    weapon.y = tileSize * Math.sin(rot) + y;
-    
-    let rotOff = -0;
+  weapon.x = tileSize * Math.cos(rot) + x;
+  weapon.y = tileSize * Math.sin(rot) + y;
+  
+  let rotOff = -0;
 
-    if (weapon.active){
-        rotOff = Math.PI/2;
-    }
+  if (weapon.active){
+    rotOff = Math.PI/2;
+  }
 
-    let dis = tileSize/4;
-    let disx = dis * Math.cos(rot + rotOff);
-    let disy = dis * Math.sin(rot + rotOff);
+  let dis = tileSize/4;
+  let disx = dis * Math.cos(rot + rotOff);
+  let disy = dis * Math.sin(rot + rotOff);
 
 
-    if (disx + x < x){
-        offsetX = -tileSize;
-        left = true;
-    }
+  if (disx + x < x){
+    offsetX = -tileSize;
+    left = true;
+  }
 
-    if (disy + y < y){
-        offsetY = - 5 * tileSize / 8;
-        up = true;
-    }
+  if (disy + y < y){
+    offsetY = - 5 * tileSize / 8;
+    up = true;
+  }
 
-    if (up && left) {
-        image = weapon.lu;
-    }
-    else if (up) {
-        image = weapon.ru;
-    }
-    else if (left) {
-        image = weapon.ld;
-    }
-    ctx.drawImage(image,x + disx + offsetX,y + disy + offsetY,tileSize,tileSize);
+  if (up && left) {
+    image = weapon.lu;
+  }
+  else if (up) {
+    image = weapon.ru;
+  }
+  else if (left) {
+    image = weapon.ld;
+  }
+  ctx.drawImage(image,x + disx + offsetX,y + disy + offsetY,tileSize,tileSize);
 
-    if (weapon.active){
-        ctx.drawImage(forceImage,weapon.x - tileSize/2, weapon.y - tileSize/2,tileSize,tileSize);
-    }
+  if (weapon.active){
+    ctx.drawImage(forceImage,weapon.x - tileSize/2, weapon.y - tileSize/2,tileSize,tileSize);
+  }
 }
 
 // create weapons
 function createSteelSword(){
-    return {
-        name: "Steel Sword",
-        x: 0,
-        y: 0,
-        active: false,
-        tic:0,
-        cool:toHit*2,
-        damage:5,
-        // textures
-        ru: swordSteelR,
-        lu: swordSteelL,
-        rd: swordSteelRD,
-        ld: swordSteelLD,
+  return {
+    name: "Steel Sword",
+    x: 0,
+    y: 0,
+    active: false,
+    tic:0,
+    cool:toHit*2,
+    damage:5,
+    // textures
+    ru: swordSteelR,
+    lu: swordSteelL,
+    rd: swordSteelRD,
+    ld: swordSteelLD,
 
-        // functions
-        use: function() {
-            if (this.tic <= 0) {
-                this.tic = this.cool;
-                this.active = true;
-            }
-        }
+    // functions
+    use: function() {
+      if (this.tic <= 0) {
+        this.tic = this.cool;
+        this.active = true;
+      }
     }
+  }
 }
 
 
 function testHit(weapon, entity, radius = tileSize){
-    if (weapon.active) {
-        // see if the weapon is within tilesize/2
-        let dis = getDistance(weapon.x, weapon.y,entity.x,entity.y);
-        return dis <= radius;
-    }
+  if (weapon.active) {
+    // see if the weapon is within tilesize/2
+    let dis = getDistance(weapon.x, weapon.y,entity.x,entity.y);
+    return dis <= radius;
+  }
 }
 
 function getDistanceBox(x,y,bx,by, width) {
-    return x >= bx && x <= bx + width && y >= by && y <= by + width
+  return x >= bx && x <= bx + width && y >= by && y <= by + width
 }
 
 function getDistance(x1,y1, x2,y2) {
-    let disX = x2 - x1;
-    let disY = y2 - y1;
-    return Math.sqrt(disX * disX + disY * disY);
+  let disX = x2 - x1;
+  let disY = y2 - y1;
+  return Math.sqrt(disX * disX + disY * disY);
 }
--- source/terrain.js
+++ source/terrain.js
@@ -25,183 +25,182 @@ var upperCliff = [];
 var tileSize = 64;
 
 function buildMap(size){
-    ground = [];
-    cliff = [];
-    upperCliff = [];
-    let x = 0;
-    let y = 0;
-    let d = randInt(4);
-    let i = 0;
-    while(size > 0) {
-        if (!isTile(x,y,ground)){
-            addGround(x,y,i);
-            size--;
-            i++;
-            d = randInt(4);
-        }
-        
-        if (d == 0){
-            x += tileSize;
-        }
-        else if (d == 1){
-            x -= tileSize;
-        }
-        else if (d == 3){
-            y += tileSize;
-        }
-        else{
-            y -= tileSize;
-        }
+  ground = [];
+  cliff = [];
+  upperCliff = [];
+  let x = 0;
+  let y = 0;
+  let d = randInt(4);
+  let i = 0;
+  while(size > 0) {
+    if (!isTile(x,y,ground)){
+      addGround(x,y,i);
+      size--;
+      i++;
+      d = randInt(4);
     }
+    
+    if (d == 0){
+      x += tileSize;
+    }
+    else if (d == 1){
+      x -= tileSize;
+    }
+    else if (d == 3){
+      y += tileSize;
+    }
+    else{
+      y -= tileSize;
+    }
+  }
 
-    sortBlocks();
-    buildCliffs();
+  sortBlocks();
+  buildCliffs();
 }
 
 function addGround(locX,locY, i){
-    let t = randInt(4) + 1;
-    ground[ground.length] = {
-        x:locX,
-        y:locY,
-        type:t,
-        index:i,
-        previous:"blank",
-        row:0
-    };
+  let t = randInt(4) + 1;
+  ground[ground.length] = {
+    x:locX,
+    y:locY,
+    type:t,
+    index:i,
+    previous:"blank",
+    row:0
+  };
 }
 
 function buildCliffs(){
-    // get top cliff first
-    ground.forEach(tile => {
-        let x = tile.x;
-        let y = tile.y -tileSize;
-        if (!isTile(x,y,ground)){
-            addCliff(x,y,"up");
-        }
-    });
+  // get top cliff first
+  ground.forEach(tile => {
+    let x = tile.x;
+    let y = tile.y -tileSize;
+    if (!isTile(x,y,ground)){
+      addCliff(x,y,"up");
+    }
+  });
 
-    // fill bottom and sides
-    ground.forEach(tile =>{
-        let x = tile.x;
-        let y = tile.y;
-        // check bottom
-        if (!isTile(x,y + tileSize,ground)) {
-            addUpper(x,y,"bottom");
-            
-            if (!isTile(x,y+tileSize,cliff)){
-                addUpper(x,y+tileSize,"side");
-            }
-        }
-        // check sides
-        if (!isTile(x + tileSize,y,ground) && !isTile(x + tileSize,y,cliff)) {
-            addUpper(x + tileSize,y,"side");
-        }
-        if (!isTile(x - tileSize,y,ground) && !isTile(x - tileSize, y, cliff)) {
-            addUpper(x -tileSize,y,"side");
-        }
-    });
+  // fill bottom and sides
+  ground.forEach(tile =>{
+    let x = tile.x;
+    let y = tile.y;
+    // check bottom
+    if (!isTile(x,y + tileSize,ground)) {
+      addUpper(x,y,"bottom");
 
-    // fill in cliff sides
-    cliff.forEach(tile =>{
-        let x = tile.x;
-        let y = tile.y;
+      if (!isTile(x,y+tileSize,cliff)){
+        addUpper(x,y+tileSize,"side");
+      }
+    }
+    // check sides
+    if (!isTile(x + tileSize,y,ground) && !isTile(x + tileSize,y,cliff)) {
+      addUpper(x + tileSize,y,"side");
+    }
+    if (!isTile(x - tileSize,y,ground) && !isTile(x - tileSize, y, cliff)) {
+      addUpper(x -tileSize,y,"side");
+    }
+  });
 
-        // check sides
-        if (!isTile(x + tileSize,y,ground) && !isTile(x + tileSize,y,cliff)) {
-            addUpper(x + tileSize,y,"side");
-        }
-        if (!isTile(x - tileSize,y,ground) && !isTile(x - tileSize, y, cliff)) {
-            addUpper(x -tileSize,y,"side");
-        }
-    });
+  // fill in cliff sides
+  cliff.forEach(tile =>{
+    let x = tile.x;
+    let y = tile.y;
+
+    // check sides
+    if (!isTile(x + tileSize,y,ground) && !isTile(x + tileSize,y,cliff)) {
+      addUpper(x + tileSize,y,"side");
+    }
+    if (!isTile(x - tileSize,y,ground) && !isTile(x - tileSize, y, cliff)) {
+      addUpper(x -tileSize,y,"side");
+    }
+  });
 }
 
 function addCliff(locX,locY,dir){
-    let t = randInt(2)+1;
-    cliff[cliff.length] = {
-        x:locX,
-        y:locY,
-        type:t,
-        d:dir
-    }
+  let t = randInt(2)+1;
+  cliff[cliff.length] = {
+    x:locX,
+    y:locY,
+    type:t,
+    d:dir
+  }
 }
 
 function addUpper(locX,locY,dir) {
-    upperCliff[upperCliff.length] = {
-        x:locX,
-        y:locY,
-        d:dir
-    }
+  upperCliff[upperCliff.length] = {
+    x:locX,
+    y:locY,
+    d:dir
+  }
 }
 
 function drawGround(){
-    ground.forEach(tile => {
-        let i = ground4;
-        if (tile.type == 1) {
-            i = ground1;
-        }
-        else if (tile.type == 2) {
-            i = ground2;
-        }
-        else if (tile.type == 3) {
-            i = ground3;
-        }
-        ctx.drawImage(i,tile.x,tile.y,tileSize,tileSize);
+  ground.forEach(tile => {
+    let i = ground4;
+    if (tile.type == 1) {
+      i = ground1;
+    }
+    else if (tile.type == 2) {
+      i = ground2;
+    }
+    else if (tile.type == 3) {
+      i = ground3;
+    }
+    ctx.drawImage(i,tile.x,tile.y,tileSize,tileSize);
 /* 
-        ctx.font = "25px Arial";
-        ctx.fillStyle = "white";
-        ctx.fillText("i:"+tile.index,tile.x,tile.y+25);
-        ctx.fillStyle = "red";
-        ctx.fillText(tile.previous,tile.x,tile.y + 51);
-        */
-    });
+    ctx.font = "25px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("i:"+tile.index,tile.x,tile.y+25);
+    ctx.fillStyle = "red";
+    ctx.fillText(tile.previous,tile.x,tile.y + 51);
+    */
+  });
 }
 
 function drawCliff() {
-    cliff.forEach(tile => {
-        let cimage;
-        if (tile.d == "up"){
-            cimage = cliff1;
-            if (tile.type == 2){
-                cimage = cliff2;
-            }
-        }
-        else if (tile.d == "side"){
-            cimage = sideCliff;
-        }
-        else{
-            cimage = bottomCliff;
-        }
-        ctx.drawImage(cimage,tile.x,tile.y,tileSize,tileSize);
-    });
+  cliff.forEach(tile => {
+    let cimage;
+    if (tile.d == "up"){
+      cimage = cliff1;
+      if (tile.type == 2){
+        cimage = cliff2;
+      }
+    }
+    else if (tile.d == "side"){
+      cimage = sideCliff;
+    }
+    else{
+      cimage = bottomCliff;
+    }
+    ctx.drawImage(cimage,tile.x,tile.y,tileSize,tileSize);
+  });
 
 }
 
 function drawUpper() {
-    upperCliff.forEach(tile => {
-        let cimage;
-        if (tile.d == "side"){
-            cimage = sideCliff;
-        }
-        else{
-            cimage = bottomCliff;
-        }
-        ctx.drawImage(cimage,tile.x,tile.y,tileSize,tileSize);
-    })
+  upperCliff.forEach(tile => {
+    let cimage;
+    if (tile.d == "side"){
+      cimage = sideCliff;
+    }
+    else{
+      cimage = bottomCliff;
+    }
+    ctx.drawImage(cimage,tile.x,tile.y,tileSize,tileSize);
+  })
 }
 
 function isTile(x, y,tile){
-    for (let i = 0; i < tile.length; i++){
-        if (tile[i].x == x && tile[i].y == y){
-            return true;
-        }
+  for (let i = 0; i < tile.length; i++){
+    if (tile[i].x == x && tile[i].y == y){
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function onGround(x,y){
-
-    return getTile(x,y) != null;
+  return getTile(x,y) != null;
 
 //    for (let i = 0; i < ground.length; i++){
 //        let tile = ground[i];
@@ -213,134 +212,133 @@ function onGround(x,y){
 }
 
 function getTile(x,y){
-    // find x first
-    if (ground.length < 1){
-        return null;
-    }
-    // liniarly find the y quardinate
-    for(let i = 0; i < ground.length; i++){
-        let tile = ground[i];
-        // test y 
-        if (tile.y + tileSize <= y) {
-            // go to next row
-            i += tile.row; 
-            continue;
-        }
-
-        // see if we match the y
-        if (tile.y + tileSize >= y) {
-            
-            if (isOnGroundTile(tile,x,y)) {
-                return tile;
-            }
-            else {
-                   
-            }
-        
-        }
-    }
+  // find x first
+  if (ground.length < 1){
     return null;
+  }
+  // liniarly find the y quardinate
+  for(let i = 0; i < ground.length; i++){
+    let tile = ground[i];
+    // test y 
+    if (tile.y + tileSize <= y) {
+      // go to next row
+      i += tile.row; 
+      continue;
+    }
+
+    // see if we match the y
+    if (tile.y + tileSize >= y) {
+      if (isOnGroundTile(tile,x,y)) {
+        return tile;
+      }
+      else {
+              
+      }
+    
+    }
+  }
+  return null;
 }
 
 function isOnGroundTile(tile, x, y) {
-    return x >= tile.x && x < tile.x + tileSize && y >= tile.y && y < tile.y + tileSize
+  return x >= tile.x && x < tile.x + tileSize && y >= tile.y && y < tile.y + tileSize
 }
 
 function randInt(max){
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 var queue = [];
 function findPath(playerX, playerY) {
-    // get the block that the player is on
-    let tile = getTile(playerX,playerY);
-    
-    findPath(tile);
+  // get the block that the player is on
+  let tile = getTile(playerX,playerY);
+  
+  findPath(tile);
 }
 
 function findPath(tile) {
-    tile.previous = "c";
-    queue = [];
+  tile.previous = "c";
+  queue = [];
 
-    // add to the queue
-    queue.push(tile);
+  // add to the queue
+  queue.push(tile);
 
-    let i = 0;
-    // pop from the queue and add until empty
-    while (i < queue.length) {
-        growQueue(i);
-        i++;
-    }
+  let i = 0;
+  // pop from the queue and add until empty
+  while (i < queue.length) {
+    growQueue(i);
+    i++;
+  }
 }
 
 function growQueue(i){
-    let tile = queue[i];
-    // get 4 adjacent tiles
-    let top = getTile(tile.x,tile.y-tileSize);
-    let bottom = getTile(tile.x,tile.y+tileSize);
-    let left = getTile(tile.x - tileSize,tile.y);
-    let right = getTile(tile.x +tileSize,tile.y);
-    
-    addQueue(top,"down");
-    addQueue(bottom,"up");
-    addQueue(left,"right");
-    addQueue(right,"left");
+  let tile = queue[i];
+  // get 4 adjacent tiles
+  let top = getTile(tile.x,tile.y-tileSize);
+  let bottom = getTile(tile.x,tile.y+tileSize);
+  let left = getTile(tile.x - tileSize,tile.y);
+  let right = getTile(tile.x +tileSize,tile.y);
+  
+  addQueue(top,"down");
+  addQueue(bottom,"up");
+  addQueue(left,"right");
+  addQueue(right,"left");
 }
 
 function addQueue(tile,dir){
-    if (tile != null) {
-        if (tile.previous == "blank") {
-            tile.previous = dir;
-            queue.push(tile);
-        }
+  if (tile != null) {
+    if (tile.previous == "blank") {
+      tile.previous = dir;
+      queue.push(tile);
     }
+  }
 }
 
 function resetPath(){
-    ground.forEach(tile => {
-        tile.previous = "blank";
-    });
+  ground.forEach(tile => {
+    tile.previous = "blank";
+  });
 }
 
 function sortBlocks(){
-    ground.sort(comparitor);
-    
-    // get the row size
-    setRowValues();
+  ground.sort(comparitor);
+  
+  // get the row size
+  setRowValues();
 }
 
 function comparitor(a,b){
-    let y = a.y - b.y;
-    if (y === 0){
-        return a.x - b.x;
-    }
-    return y;
+  let y = a.y - b.y;
+  if (y === 0){
+    return a.x - b.x;
+  }
+  return y;
 }
 
 function setRowValues() {
-    if (ground.length < 1){
-        return;
+  if (ground.length < 1){
+    return;
+  }
+  ground[0].index = 0;
+  let y = ground[0].y;
+  let start = 0;
+  let end = 0;
+  for (let i = 1; i < ground.length; i++){
+    ground[i].index = i;
+    if (ground[i].y == y) {
+      end++;
     }
-    ground[0].index = 0;
-    let y = ground[0].y;
-    let start = 0;
-    let end = 0;
-    for (let i = 1; i < ground.length; i++){
-        ground[i].index = i;
-        if (ground[i].y == y) {
-            end++;
-        }
-        else{
-            fillRow(start,end);
-            y = ground[i].y;
-            start = i;
-            end = i;
-        }
+    else{
+      fillRow(start,end);
+      y = ground[i].y;
+      start = i;
+      end = i;
     }
+  }
 }
 
 function fillRow(startIndex, endIndex){
-    for (let i = startIndex; i <= endIndex; i++) {
-        ground[i].row = endIndex - i;
-    }
+  for (let i = startIndex; i <= endIndex; i++) {
+    ground[i].row = endIndex - i;
+  }
 }
